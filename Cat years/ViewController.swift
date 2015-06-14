ef90d2498f1a1a1a3cb57896a91c265243d9e4c6
Fran Primo Ruiz <FranPrimo@MacBook-de-Fran.local>
2015-06-14 19:47:30 +0200

    Initial Commit

---
+++ Cat years/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cat years
+//
+//  Created by Fran Primo Ruiz on 14/6/15.
+//  Copyright (c) 2015 Fran Primo Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
