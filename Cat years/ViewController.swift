04e6aafc4c804d6e00949eed13f2e201832757a7
Fran Primo Ruiz <FranPrimo@MacBook-de-Fran.local>
2015-06-14 20:28:40 +0200

    Probando

--- Cat years/ViewController.swift
+++ Cat years/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    @IBOutlet weak var enteredAge: UITextField!
+    
+    @IBAction func sendButton(sender: AnyObject) {
+        
+        var catAge = enteredAge.text.toInt()
+        if(catAge != nil){
+            var resultAge = catAge! * 7
+            resultLbl.text = "Your cat is \(resultAge) years old"
+        }else{
+            resultLbl.text = "Enter the age of your cat in the box"
+        }
+
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
